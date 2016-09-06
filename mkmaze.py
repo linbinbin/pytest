3425bc4f8abbf7f53555af76f357bbc8067e74b5
linbinbin <lins01jp@gmail.com>
2016-09-06 09:48:48 +0900

    make big maze

--- mkmaze.py
+++ mkmaze.py
@@ -21,9 +21,9 @@ def readmap(filename, matrix, path):
             for col, w in enumerate(line):
                 node = Node(row, col, w)
                 r.append(node)
-                if w == 's':
+                if w == 'S':
                     path.append(node)
-                    print('add ', node.x, node.y)
+                    #print('add ', node.x, node.y)
             matrix.append(r)
 
 def mkmeiro(matrix, path):
@@ -31,34 +31,28 @@ def mkmeiro(matrix, path):
     mv, dis, p = get_dir_dis()
     counter = 0
     t = 0
-    w = 0
-    v = 0
+    w = 1
     directions = [(-1, 0),(0, 1),(1, 0),(0, -1)]
     while True:
         # check erea
         if current.x+directions[mv][0]*2 > 0 and  current.x+directions[mv][0]*2 < len(matrix[0])-2 and current.y+directions[mv][1]*2 > 0 and  current.y+directions[mv][1]*2 < len(matrix)-1:
-                if matrix[current.y+directions[mv][1]*2][current.x+directions[mv][0]*2].val == 't':
+                if matrix[current.y+directions[mv][1]*2][current.x+directions[mv][0]*2].val == 'G':
                     matrix[current.y+directions[mv][1]][current.x+directions[mv][0]].val = ' '
                     t = 1
-                elif matrix[current.y+directions[mv][1]*2][current.x+directions[mv][0]*2].val == 'w':
+                elif matrix[current.y+directions[mv][1]*2][current.x+directions[mv][0]*2].val == 'T':
                     matrix[current.y+directions[mv][1]][current.x+directions[mv][0]].val = ' '
                     w = 1
-                elif matrix[current.y+directions[mv][1]*2][current.x+directions[mv][0]*2].val == 'v':
+                elif matrix[current.y+directions[mv][1]*2][current.x+directions[mv][0]*2].val != ' ' and matrix[current.y+directions[mv][1]*2][current.x+directions[mv][0]*2].val != 'S':
                     matrix[current.y+directions[mv][1]][current.x+directions[mv][0]].val = ' '
-                    v = 1
-                elif matrix[current.y+directions[mv][1]*2][current.x+directions[mv][0]*2].val != '*':
-                    current = path[random.randint(0, len(path)-1)]
-                    mv, dis, p = get_dir_dis()
-                    #print('2')
-                else:
-                    matrix[current.y+directions[mv][1]][current.x+directions[mv][0]].val = 'p' if p>3 else ' '
-                    matrix[current.y+directions[mv][1]*2][current.x+directions[mv][0]*2].val = 'p' if p>2 else ' '
+                    matrix[current.y+directions[mv][1]*2][current.x+directions[mv][0]*2].val = 'P' if p>4 else ' '
                     current = matrix[current.y+directions[mv][1]*2][current.x+directions[mv][0]*2]
                     path.append(current)
                     counter += 1
-                    mv, dis, p = get_dir_dis()
+                else:
+                    current = matrix[current.y+directions[mv][1]*2][current.x+directions[mv][0]*2]
+                mv, dis, p = get_dir_dis()
                     #print('3')
-                if t and w and v:                  
+                if t and w:                  
                     break
                 else:
                     mv, dis, p = get_dir_dis()
@@ -72,12 +66,12 @@ def mkmeiro(matrix, path):
         for w in line:
             enstr += w.val
     print(enstr)
-    print(counter)
+    #print(counter)
   
 if __name__ == '__main__':
     mat = []
     meiro = []
-    readmap("map.txt", mat, meiro)
+    readmap("map_b.txt", mat, meiro)
     mkmeiro(mat, meiro)
 
 
